386543e3236221e2efb0c5d1c5902c79e5ecbd28
nicole <nicole915@sina.com>
2018-11-03 07:41:53 +0800

    test

---
+++ ActivityIndicatorApp/ActivityIndicatorApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ActivityIndicatorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Navigation.PushAsync(new NewPage());
+        }
+    }
+}
